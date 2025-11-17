0bc40fdc613c7916a8ab0cc1573a03dd9a726d6d
abassadebayo <greatmoh007@gmail.com>
2025-11-17 11:00:18 +0100

    array class work correction

--- fundamental-classes/Program.cs
+++ fundamental-classes/Program.cs
@@ -1,60 +1,25 @@
-﻿Console.Write("Enter length for array1: ");
-int length1 = Convert.ToInt32(Console.ReadLine());
+﻿int[] numbers = { 3, 3, 3, 1, 2, 1, 2, 3, 3, 2, 2, 1, 3, 1, 3, 1, 2, 2, 3, 3, 3 };
+int value = 0;
+int count = 0;
 
-char[] array1 = new char[length1];
-
-Console.Write("Enter length for array2: ");
-int length2 = Convert.ToInt32(Console.ReadLine());
-
-char[] array2 = new char[length2];
-
-
-int length = int.Min(length1, length2);
-
-Console.WriteLine("Enter elements for array1: ");
-for(int i = 0; i < array1.Length; i++)
+for (int i = 0; i < numbers.Length; i++)
 {
-    Console.Write($"Array1[{i}]: ");
-    array1[i] = Convert.ToChar(Console.ReadLine());
-}
+    int currentCount = 1;
 
-Console.WriteLine("Enter elements for array2: ");
-for(int i = 0; i < array2.Length; i++)
-{
-    Console.Write($"Array2[{i}]: ");
-    array2[i] = Convert.ToChar(Console.ReadLine());
-}
-
-string result = "";
-
-for(int i = 0; i < length; i++)
-{
-    if(array1[i] > array2[i])
+    for(int j  = i+ 1; j < numbers.Length; j++)
     {
-        result = "Array2 comes first in lexicographical order";
-        break;
+        if(numbers[i] == numbers[j])
+        {
+            currentCount++;
+        }
     }
-    else if(array1[i] < array2[i])
+    
+    if(currentCount > count)
     {
-        result = "Array1 comes first in lexicographical order";
-        break;
-    }
-    else
-    {
-        if(array1.Length > array2.Length)
-        {
-            result = "Array2 comes first in lexicographical order";
-        }
-        else if(array1.Length < array2.Length)
-        {
-            result = "Array1 comes first in lexicographical order";
-        }
-        else
-        {
-            result = "Array1 and Array2 are equal";
-        }
+        count = currentCount;
+        value = numbers[i];
+
     }
 }
 
-
-Console.WriteLine(result);
+Console.WriteLine($"{value} appears {count} times");
