8925b96ad0d351d50eee601dc3e8d068a7df5341
abassadebayo <greatmoh007@gmail.com>
2025-11-20 13:23:23 +0100

    2D Array class

--- fundamental-classes/Program.cs
+++ fundamental-classes/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.Write("Enter the length for row: ");
+﻿Console.Write("Enter length for layer: ");
+int layers;
+
+Console.Write("Enter the length for row: ");
 int rows;
 
 while(!int.TryParse(Console.ReadLine(), out rows))
@@ -14,28 +17,20 @@ while (!int.TryParse(Console.ReadLine(), out columns))
     Console.WriteLine("Enter valid value for colums!");
 }
 
+
 int[,] arr = new int[rows, columns];
 
 Console.WriteLine("Enter element for rows and colums...");
-for (int i = 0; i < rows; i++)
-{
-    for(int j = 0; j < columns; j++)
+
+    for (int i = 0; i < rows; i++)
     {
-        Console.Write($"arr[{i}, {j}]: ");
-        arr[i, j] = Convert.ToInt32(Console.ReadLine());
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write($"arr[{i}, {j}]: ");
+            arr[i, j] = Convert.ToInt32(Console.ReadLine());
+        }
     }
-}
 
-int sum = 0;
-for (int i = 0; i < rows; i++)
-{
-    for (int j = 0; j < columns; j++)
-    {
-        Console.Write(arr[i, j] + " ");
-        sum += arr[i, j];
 
-    }
-    Console.WriteLine();
-}
 
-Console.WriteLine($"The sum of the {rows} x {columns} matrix = {sum}");
+
